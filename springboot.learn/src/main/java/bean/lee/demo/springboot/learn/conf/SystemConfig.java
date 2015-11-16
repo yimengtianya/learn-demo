916d39f1682f9f6c2cd654aac64a146bcfb50ddd
Dube <libinsbox@163.com>
2015-11-16 19:28:48 +0800

    Dube: add zk

--- springboot.learn/src/main/java/bean/lee/demo/springboot/learn/conf/SystemConfig.java
+++ springboot.learn/src/main/java/bean/lee/demo/springboot/learn/conf/SystemConfig.java
@@ -1,12 +1,20 @@
 package bean.lee.demo.springboot.learn.conf;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * 从zk上同步配置
  * 
  * @author Dube
  * @date 2015年11月14日 下午5:22:59
  */
+@Configuration
+@EnableDiscoveryClient
 public class SystemConfig {
 
+	@Value("${spring.application.name:testZookeeperApp}")
+	private String appname;
 
 }
