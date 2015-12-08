8e6bbda785705a67e7f83ab88038058435c60f08
Dube <libinsbox@163.com>
2015-12-08 19:12:16 +0800

    Dube: 消息增加ID

--- mqtt-message-server/src/main/java/bean/lee/push/web/entity/Message.java
+++ mqtt-message-server/src/main/java/bean/lee/push/web/entity/Message.java
@@ -3,15 +3,20 @@ package bean.lee.push.web.entity;
 public class Message {
 
 	/**
+	 * id
+	 */
+	private long id;
+
+	/**
 	 * 主题
 	 */
 	private String topic;
-	
+
 	/**
 	 * 内容
 	 */
 	private String content;
-	
+
 	/**
 	 * 存活时间，单位：秒
 	 */
@@ -40,7 +45,13 @@ public class Message {
 	public void setLifetime(Long lifetime) {
 		this.lifetime = lifetime;
 	}
-	
-	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 }
