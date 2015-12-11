4b76c1e58d4d6cb0a65b1f790eb0a20fa14c4abc
Dube <libinsbox@163.com>
2015-12-11 13:47:06 +0800

    Dube: Java8学习

---
+++ java8.learn/src/main/java/bean/lee/demo/java8/learn/common/Artist.java
@@ -0,0 +1,26 @@
+package bean.lee.demo.java8.learn.common;
+
+/**
+ * 艺术家
+ * 
+ * @author Dube
+ * @date 2015年12月9日 下午12:28:01
+ */
+public class Artist {
+	
+	/**
+	 * 名称
+	 */
+	private String name;
+	
+	/**
+	 * 成员数量
+	 */
+	private int members;
+	
+	/**
+	 * 地区
+	 */
+	private String origin;
+
+}
