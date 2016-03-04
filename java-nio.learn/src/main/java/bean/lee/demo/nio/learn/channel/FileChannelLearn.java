79a2272a40e3b01de4e4b1c22c4dd38d19c32462
Dube <libinsbox@163.com>
2016-03-04 12:55:37 +0800

    Dube:java-nio Channel

---
+++ java-nio.learn/src/main/java/bean/lee/demo/nio/learn/channel/FileChannelLearn.java
@@ -0,0 +1,41 @@
+package bean.lee.demo.nio.learn.channel;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileChannelLearn {
+
+	public static void main(String[] args) throws IOException {
+
+		RandomAccessFile aFile = new RandomAccessFile("data/nio-data.txt", "rw");
+		FileChannel inChannel = aFile.getChannel();
+
+		ByteBuffer buf = ByteBuffer.allocate(48);
+
+		// 读取到Buffer
+		int bytesRead = inChannel.read(buf);
+
+		while (bytesRead != -1) {
+
+			//System.out.println("Read " + bytesRead);
+
+			// 反转Buffer
+			buf.flip();
+
+			while (buf.hasRemaining()) {
+				// 从Buffer中读数据
+				System.out.print((char) buf.get());
+			}
+
+			buf.clear();
+
+			// 读取到Buffer
+			bytesRead = inChannel.read(buf);
+		}
+		aFile.close();
+
+	}
+
+}
